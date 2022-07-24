664ccc29bc60e0f99623be048400b0d0f49bf07d
EzequielK-source <programacionezequielk@gmail.com>
2022-07-23 21:52:45 -0300

    (Test) Add test for POST a txt file format

--- test/route/file-route.test.js
+++ test/route/file-route.test.js
@@ -1,11 +1,13 @@
-//Test module configs
+//Test and module configs
 const chai = require('chai')
     , chaiHttp = require('chai-http')
 chai.use(chaiHttp);
 const {expect, request} = chai;
-
+//Modules import
+const fs = require('fs')
 //App domain imports
 const FilesRouter = require('src/route/files-router');
+
 const app = require('src/app')
 describe('FileRouter test', () => {
     it('Status test', (done) => {
@@ -21,16 +23,18 @@ describe('FileRouter test', () => {
             })
     });
 
-    it('POST request', (done) => {
-        request(app)
-            .post('/files')
-            .set('content-type','json-application')
-            .end((err,res)=>{
-                if(err) done(err);
-
-                expect(res).to.have.status(201);
-                expect(res).to.be.json;
-                done()
-            })
+    describe('POST request', () => {
+        it('Send txt file', (done) => {
+            request(app)
+                .post('/files')
+                .attach('file',fs.readFileSync('test/files/aTxtFile.txt'), 'aTxtFile.txt')
+                .end((err,res)=>{
+                    if(err) done(err);
+                    expect(res).to.have.status(201);
+                    expect(res).to.be.json;
+                    expect(res.body).to.be.equal('The file "aTxtFile.txt" was saved.')
+                    done()
+                })
+        });
     });
 });
