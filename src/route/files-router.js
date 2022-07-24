1694827d3466d7a7f49776978ff25e2c78c4b02e
EzequielK-source <programacionezequielk@gmail.com>
2022-07-23 21:24:43 -0300

    (Feature) Add post method to FilesRouter

--- src/route/files-router.js
+++ src/route/files-router.js
@@ -1,9 +1,13 @@
 const Router = require('express').Router;
 const FileRouter = Router();
 
-FileRouter.get('/',(req,res)=>{
-    return res.status(200).json('file router services on')
-})
+FileRouter.route('/')
+    .get((req, res) => {
+        return res.status(200).json('file router services on')
+    })
+    .post((req,res)=>{
+        return res.status(201).json('file saved')
+    })
 
 
 module.exports = FileRouter;
