ddb5eb0c01a1206fb2153e56129a3262ce20bba0
EzequielK-source <programacionezequielk@gmail.com>
2022-08-17 17:43:28 -0300

    Modified FileRouter to accept files and save it in server
    - Now FileRouter test verify POST /files route response
    - Add config module
    - Create teting config

--- src/route/files-router.js
+++ src/route/files-router.js
@@ -1,13 +1,17 @@
 const Router = require('express').Router;
 const FileRouter = Router();
 
+const SaveFileInServer = require('src/services/save-file-in-server')
 FileRouter.route('/')
     .get((req, res) => {
         return res.status(200).json('File router services on')
     })
-    .post((req,res)=>{
+    .post(async(req,res)=>{
         const fileToSave = req.files.file;
-        return res.status(201).json(`The file "${fileToSave.name}" was saved.`)
+        await SaveFileInServer(fileToSave);
+        return res.status(201).json({
+            status:`The file ${fileToSave.name} was stored in server`
+        })
     })
 
 
--- test/route/file-route.test.js
+++ test/route/file-route.test.js
@@ -1,40 +1,43 @@
 //Test and module configs
 const chai = require('chai')
     , chaiHttp = require('chai-http')
-chai.use(chaiHttp);
-const {expect, request} = chai;
+    , deepEqualInAnyOrder = require('deep-equal-in-any-order')
+chai.use(chaiHttp)
+chai.use(deepEqualInAnyOrder)
+const { expect, request } = chai
 //Modules import
 const fs = require('fs')
 //App domain imports
-const FilesRouter = require('src/route/files-router');
-
 const app = require('src/app')
 describe('FileRouter test', () => {
     it('Status test', (done) => {
         request(app)
             .get('/files')
-            .set('content-type','json-application')
-            .end((err,res)=>{
-                if(err) done(err)
+            .set('content-type', 'json-application')
+            .end((err, res) => {
+                if (err) done(err)
 
                 expect(res).to.have.status(200)
-                expect(res).to.be.json;
-                done();
+                expect(res).to.be.json
+                done()
             })
-    });
-
+    })
     describe('POST request', () => {
         it('Send txt file', (done) => {
             request(app)
                 .post('/files')
-                .attach('file',fs.readFileSync('test/files/aTxtFile.txt'), 'aTxtFile.txt')
-                .end((err,res)=>{
-                    if(err) done(err);
-                    expect(res).to.have.status(201);
-                    expect(res).to.be.json;
-                    expect(res.body).to.be.equal('The file "aTxtFile.txt" was saved.')
+                .attach('file', fs.readFileSync('test/files/aTxtFile.txt'), 'aTxtFile.txt')
+                .end((err, res) => {
+                    if (err) done(err)
+                    expect(res).to.have.status(201)
+                    expect(res).to.be.json
+                    expect(res.body)
+                        .to
+                        .have
+                        .property('status')
+                        .equal('The file aTxtFile.txt was stored in server')
                     done()
                 })
-        });
-    });
-});
+        })
+    })
+}) 
