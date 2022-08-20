edd9e7b7aba0b9146dfd66a1a724f05110324919
EzequielK-source <programacionezequielk@gmail.com>
2022-08-19 21:37:17 -0300

    Remove async in FileRouter post method -Update .gitignore -Tesst passed

--- src/route/files-router.js
+++ src/route/files-router.js
@@ -4,11 +4,13 @@ const FileRouter = Router();
 const SaveFileInServer = require('src/services/save-file-in-server')
 FileRouter.route('/')
     .get((req, res) => {
-        return res.status(200).json('File router services on')
+        return res.status(200).json({
+            files: []
+        })
     })
-    .post(async(req,res)=>{
+    .post((req,res)=>{
         const fileToSave = req.files.file;
-        await SaveFileInServer(fileToSave);
+        SaveFileInServer(fileToSave, "testUUID");
         return res.status(201).json({
             status:`The file ${fileToSave.name} was stored in server`
         })
